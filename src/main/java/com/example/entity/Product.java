9edd58133829c9406fb932ea6131b54b1a170bfd
suzuki <moo00chan2525@gmail.com>
2026-04-28 16:25:59 +0900

    Thymeleafフラグメントを導入し、共通レイアウト（ヘッダー・フッター）を作成

---
+++ src/main/java/com/example/entity/Product.java
@@ -0,0 +1,40 @@
+package com.example.entity;
+
+public class Product {
+
+	private int id;
+	private String name;
+	private int price;
+
+	public Product(int id, String name, int price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setNameString(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
